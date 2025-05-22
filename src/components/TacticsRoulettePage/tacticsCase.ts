3daed6e8b3fbc4934018c3a39f89e4bec821324e
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-05-22 10:59:38 +0300

    add map property to tactics

--- src/components/TacticsRoulettePage/tacticsCase.ts
+++ src/components/TacticsRoulettePage/tacticsCase.ts
@@ -1,10 +1,20 @@
-export const tacticsCase = [
+import type { MapName } from "../MapRoulette/mapCase";
+import type { CaseItem } from "../roulette/types";
+
+export type TacticMapContext = MapName | "Shared";
+
+export interface Tactic extends CaseItem {
+	map: TacticMapContext;
+}
+
+export const tacticsCase: readonly Tactic[] = [
 	{
 		id: 1,
 		name: "RELLU KOURAA",
 		image: "/tactics/vanha-käppänä.png",
 		rarity: "Rare Special Item",
 		weight: 1,
+		map: "Shared",
 	},
 	{
 		id: 2,
@@ -12,6 +22,7 @@ export const tacticsCase = [
 		image: "/tactics/bruhpepe.jpg",
 		rarity: "Mil-spec",
 		weight: 1,
+		map: "Shared",
 	},
 	{
 		id: 3,
@@ -19,14 +30,16 @@ export const tacticsCase = [
 		image: "/tactics/trollface.png",
 		rarity: "Restricted",
 		weight: 1,
+		map: "Shared",
 	},
 	{
 		id: 4,
 		name: "RUSH MID",
-		image: "",
+		image: "123",
 		rarity: "Mil-spec",
 		weight: 1,
+		map: "Shared",
 	},
-] as const;
+];
 
 export type TacticName = (typeof tacticsCase)[number]["name"];
