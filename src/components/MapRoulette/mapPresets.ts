026deab4f3855cf084960131f58902359bc631f9
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-05-23 10:33:33 +0300

    äijien ilta map preset

--- src/components/MapRoulette/mapPresets.ts
+++ src/components/MapRoulette/mapPresets.ts
@@ -17,4 +17,23 @@ export const mapPresets: RoulettePreset<MapName>[] = [
 			{ name: "Mirage", weight: 1 },
 		],
 	},
+	{
+		name: "ÄIJIEN ILTA",
+		items: [
+			{ name: "Dust II", weight: 1 },
+			{ name: "Mirage", weight: 1 },
+			{ name: "Inferno", weight: 1 },
+			{ name: "Vertigo", weight: 1 },
+			{ name: "Train", weight: 1 },
+			{ name: "Anubis", weight: 1 },
+			{ name: "Ancient", weight: 1 },
+			{ name: "Overpass", weight: 1 },
+			{ name: "Nuke", weight: 1 },
+			{ name: "Jura", weight: 1 },
+			{ name: "Grail", weight: 1 },
+			{ name: "Agency", weight: 1 },
+			{ name: "Office", weight: 1 },
+			{ name: "Italy", weight: 1 },
+		],
+	},
 ];
